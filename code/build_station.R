d99c402d285136e4ab73eb436d2a6da65ae479f6
hirokazuhara <5123046b@g.hit-u.ac.jp>
2025-11-06 16:07:44 +0900

    新幹線停車駅定義作成

--- code/build_station.R
+++ code/build_station.R
@@ -1,6 +1,20 @@
 #build_station.R
 
 #path
-station_gj <- "spat_analysis/data_raw/ksj_n02_railway/N02-24_Station.geojson" #鉄道データ
-adm_dir    <- "spat_analysis/data_raw/ksj_n03"　#行政界データ（ディレクトリ）
-out_gj     <- "spat/analysis/data_fmt/tokaidostation_withflags.geojson" #出力先
+STATION_GJ <- "spat_analysis/data_raw/ksj_n02_railway/N02-24_Station.geojson" #鉄道データ
+ADM_DIR    <- "spat_analysis/data_raw/ksj_n03"　#行政界データ（ディレクトリ）
+OUT_GJ     <- "spat/analysis/data_fmt/tokaidostation_withflags.geojson" #出力先
+#dir.create(dirname(OUT_GJ), showWarnings = FALSE, recursive = TRUE)
+
+#駅名等表記揺れ対応措置
+norm_ja <- function(x){
+  x %>%
+    stringi::stri_trans_nfkc() %>%                     # 全角半角
+    str_replace_all("（.*?）|\\(.*?\\)", "") %>%       # 括弧除去
+    str_replace_all("駅", "") %>%
+    str_replace_all("[ 　\t]", "") %>%
+    str_replace_all("・", "") %>%
+    str_replace_all("−|―|ー", "-") %>%
+    str_to_lower()
+}
+
