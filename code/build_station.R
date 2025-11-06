102de69f990ddd8766c386ca511c918aff3ca6ca
hirokazuhara <5123046b@g.hit-u.ac.jp>
2025-11-06 15:20:00 +0900

    パス通した

--- code/build_station.R
+++ code/build_station.R
@@ -1,1 +1,6 @@
-#format_station.R
+#build_station.R
+
+#path
+station_gj <- "spat_analysis/data_raw/ksj_n02_railway/N02-24_Station.geojson" #鉄道データ
+adm_dir    <- "spat_analysis/data_raw/ksj_n03"　#行政界データ（ディレクトリ）
+out_gj     <- "spat/analysis/data_fmt/tokaidostation_withflags.geojson" #出力先
